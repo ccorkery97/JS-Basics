2d985a5ac9fc9f30e035ecce754eb6c6a7ad041a
Conor Corkery <conorcorkery@Conors-MacBook-Air.local>
2017-10-09 15:57:08 -0500

    finished

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,13 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+function isTyler(name) {
+  if (name == 'Tyler') {
+    return true;
+  } else {
+    return false;
+  }
+}
 //////////////////PROBLEM 2////////////////////
 
 
@@ -13,7 +19,10 @@ var name = 'Tyler';
 
 
   //Code Here
-
+function getName() {
+  var name = prompt('what is your name');
+  return name;
+}
 
 //////////////////PROBLEM 3////////////////////
 
@@ -23,7 +32,11 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
+function welcome() {
+  var name = getName();
+  alert("Welcome, " + name);
 
+}
 
 //////////////////PROBLEM 4////////////////////
 
@@ -33,7 +46,7 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
-
+//parameters are given to functions but arguments are passed when calling the function.
 
 //////////////////PROBLEM 5////////////////////
 
@@ -44,7 +57,14 @@ var name = 'Tyler';
 
   //Answer Here
 
-
+//The falsy values are: false, 0, '', undefined, null, NaN.
+function tOrf(value){
+  if (value == true){
+    console.log('truthy');
+  }else{
+    console.log('falsey');
+  }
+}
 
 //////////////////PROBLEM 6////////////////////
 
@@ -54,15 +74,17 @@ var name = 'Tyler';
 
   //Code Here
   
-
+function myName(){
+  return "Conor";
+}
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = myName;
 //Now alert the result of invoking newMyName
 
-
+newMyName();
 
 //////////////////PROBLEM 7////////////////////
 
@@ -71,9 +93,14 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn() {
+  return function(){
+    return "Conor";
+  }
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+  var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
